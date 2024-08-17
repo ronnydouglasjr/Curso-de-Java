5e8782dbc828bb5f056b48dc2e614dfc0b3d5da1
Ronny <ronnydougla2@gmail.com>
2024-08-17 11:06:31 -0300

    feature/ Numeros pares

---
+++ src/Exercicios/numerosPares/index.java
@@ -0,0 +1,11 @@
+package Exercicios.numerosPares;
+
+public class index {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
