cfff3ee3183ba293a20aa4d68438352ae9ad5fce
Ronny <ronnydougla2@gmail.com>
2024-08-19 16:28:13 -0300

    feature/ Soma de Ímpares Consecutivos II

--- src/Exercicios/Sequencia3/index.java
+++ src/Exercicios/Sequencia3/index.java
@@ -2,10 +2,9 @@ package Exercicios.Sequencia3;
 
 public class index {
     public static void main(String[] args) {
-        for (int i = 1; i <= 9; i+= 2) {
-            for (int j = 7; j <= 13; j--) {
+        for (int i = 1; i <= 9; i+=2) {
+            for (int j = i + 6; j >= i + 4; j--) {
                 System.out.printf("I=%d J=%d\n", i, j);
-
             }
         }
     }
